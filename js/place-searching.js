2b3818dfc7b94d7cba09cc68ae5f7101ecffec69
Amrit Singh <asingh2066@gmail.com>
2019-12-07 16:06:11 -0500

    trying to allow location access

--- js/place-searching.js
+++ js/place-searching.js
@@ -2,9 +2,34 @@ var map;
 
 function createMap () {
   var options = {
-    center: { lat: 40.730610, lng: -73.935242 },
-    zoom: 13
+    center: { lat: 40.8194, lng: -73.95 },
+    zoom: 16
   };
+
+  //trying to get ur location
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(function (p) {
+        var position = {
+          lat: p.coords.latitude,
+          lng: p.coords.longitude
+        };
+
+        infoWindow.setPosition(position);
+        infoWindow.setContent('Your location!');
+        infoWindow.open(map);
+        map.setCenter(position);
+      }, function () {
+        handleLocationError('Geolocation service failed', map.getCenter());
+      });
+    } else {
+      handleLocationError('No geolocation available.', map.getCenter());
+    }
+
+    function handleLocationError (content, position) {
+      infoWindow.setPosition(position);
+      infoWindow.setContent(content);
+      infoWindow.open(map);
+    }
 
   map = new google.maps.Map(document.getElementById('map'), options);
 
